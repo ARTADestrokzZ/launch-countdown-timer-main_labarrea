acf0346dddd267c7cf66a9be937ee5d0ec3eacf7
Antoine Labarre <2240806.csfoy.ca>
2024-12-07 11:08:14 -0500

    Mise à jour final du projet

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const uneHeureEnMs = 1000 * 60 * 60;
 const uneMinuteEnMs = 1000 * 60;
 
 //const dateCible = Date.now() - dateOffsetEnMinutes * uneMinuteEnMs + 3000;
-const dateCible = new Date("Dec 5, 2024");
+const dateCible = new Date("Dec 31, 2024");
 
 const getCountdown = () => {
     let nowDate = Date.now();
@@ -50,4 +50,3 @@ const getCountdown = () => {
 let countdownInterval = setInterval(getCountdown, 1000);
 
 getCountdown();
-
